501b5b41e5cf8c3849feb03426936fbe8a1c03a2
randomplayables <randomplayables@proton.me>
2025-07-26 23:42:24 +0000

    feat: Add src/constants/index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const MIN_NUMBER = 1
+export const MAX_NUMBER = 100
+
+export const GAUNTLET_MODE_PARAM = 'gauntlet_mode'
+export const GAUNTLET_ID_PARAM = 'gauntletId'
+
+export const GAUNTLET_MODE_CHALLENGER = 'challenger'
+export const GAUNTLET_MODE_OPPONENT = 'opponent'
+
+export const URL_PARAM_AUTH_TOKEN = 'authToken'
+export const URL_PARAM_USER_ID = 'userId'
+export const URL_PARAM_USERNAME = 'username'
+export const URL_PARAM_SURVEY_MODE = 'surveyMode'
+export const URL_PARAM_QUESTION_ID = 'questionId'
